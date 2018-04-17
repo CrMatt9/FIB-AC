f31a667523f3b6a60ad9bccc0cf4cb402cd3ea23
Marc <marc.casellas@est.fib.upc.edu>
2018-04-17 15:51:25 +0200

    Pràctica 6

--- Pràctiques/Practica6/MiSimulador.c
+++ Pràctiques/Practica6/MiSimulador.c
@@ -1,31 +1,44 @@
 #include "CacheSim.h"
+#include <stdio.h>
 
 /* Posa aqui les teves estructures de dades globals
  * per mantenir la informacio necesaria de la cache
  * */
+ 
 
+typedef struct {
+	unsigned int v; 	// Bit validesa
+	int tag;			// Etiqueta guardada a la cache
+} linia_cache;
 
+linia_cache cache[128];
 
+int hit_count;			// Contador hits
+int miss_count;			// Contador misses
 
 /* La rutina init_cache es cridada pel programa principal per
  * inicialitzar la cache.
  * La cache es inicialitzada al començar cada un dels tests.
  * */
-void init_cache ()
-{
+void init_cache () {
 	/* Escriu aqui el teu codi */
 
-
-
+	int i;
+	for (i = 0; i < 128; i++)
+		cache[i].v = 0; // Posem tots els bits de validesa a 0
+		
 }
 
 /* La rutina reference es cridada per cada referencia a simular */ 
-void reference (unsigned int address, unsigned int LE)
-{
+void reference (unsigned int address, unsigned int LE) {
+	
+	// Caché amb Write Through + Write No Allocate
+	
 	unsigned int byte;
 	unsigned int bloque_m; 
 	unsigned int linea_mc;
 	unsigned int tag;
+	
 	unsigned int miss;
 	unsigned int lec_mp;
 	unsigned int mida_lec_mp;
@@ -33,20 +46,74 @@ void reference (unsigned int address, unsigned int LE)
 	unsigned int mida_esc_mp;
 	unsigned int replacement;
 	unsigned int tag_out;
+	
+	lec_mp = 0;
+	esc_mp = 0;
+	mida_lec_mp = 0;
+	mida_esc_mp = 0;
 
 	/* Escriu aqui el teu codi */
 
+	byte = (address & 0x0000001F);				// bit 27 - 31
+	bloque_m = address >> 5;					// bit 0 - 26
+	linea_mc = (address & 0x00000FE0) >> 5;		// bit 20 - 26
+	tag = address >> 12;						// bit 0 - 19
 
-
-
-
-
-
-
-
-
-
-
+		// Dada vàlida
+	if (cache[linea_mc].v == 1) {
+		
+		// Coincideix el TAG --> HIT
+		if (cache[linea_mc].tag == tag) { 
+			
+			miss = 0;
+			replacement = 0;
+			
+			if (LE == 1) { // Escriptura
+				esc_mp = 1;
+				mida_esc_mp = 1;
+			}
+			
+			// Lectura no llegeix de memòria principal
+		}
+		// NO coincideix el TAG --> MISS
+		else {
+			miss = 1;
+			
+			
+			if (LE == 0) { // Lectura
+				replacement = 1;
+				lec_mp = 1;
+				mida_lec_mp = 32;
+				tag_out = cache[linea_mc].tag;
+				cache[linea_mc].tag = tag;
+			}
+			else { // Escriptura
+				esc_mp = 1;
+				mida_esc_mp = 1;
+			}
+		}
+	}
+	
+	// Dada no valida --> MISS (primer acces)
+	else {
+		
+		miss = 1;
+		replacement = 0;
+		
+		
+		
+		if (LE == 0) { // Lectura
+			lec_mp = 1;
+			mida_lec_mp = 32;
+			cache[linea_mc].v = 1;
+			cache[linea_mc].tag = tag;
+			
+		}
+		else { // Escriptura
+			esc_mp = 1;
+			mida_esc_mp = 1;
+		}
+	}
 
 
 
@@ -60,9 +127,8 @@ void reference (unsigned int address, unsigned int LE)
 }
 
 /* La rutina final es cridada al final de la simulacio */ 
-void final ()
-{
- 	/* Escriu aqui el teu codi */ 
-  
-  
+
+void final () {
+ 
+	printf("Fi");
 }
